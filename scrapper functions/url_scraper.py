4190f8466628f07810e81152a353b0b76180d537
Daniel Ng <67475648+daanielng@users.noreply.github.com>
2021-06-10 02:41:09 +0800

    Update url_scraper.py

--- scrapper functions/url_scraper.py
+++ scrapper functions/url_scraper.py
@@ -15,20 +15,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-username = 'alexandertham95@gmail.com'
-password = 'AT95password'
+username = 'type email'
+password = 'type password'
 
 
 
 # Logging in
-driver = webdriver.Chrome('/Users/danielng/Documents/Coding/Data Science:Analytics Stuff/Useful notebooks/Web Scraping on Linkedin/chromedriver')
-# driver.get('https://www.linkedin.com/login')
-# elementID = driver.find_element_by_id('username')
-# elementID.send_keys(username)
-# elementID = driver.find_element_by_id('password')
-# elementID.send_keys(password)
-
-# elementID.submit()
+driver = webdriver.Chrome('path to chromedriver')
 
 #google search linkedin URLs
 universities = ['National University of Singapore', 'Singapore Management University', 'Nanyang Technological University']
@@ -38,9 +31,6 @@ search_query.send_keys('site:linkedin.com/in/ AND "National University of Singap
 search_query.send_keys(Keys.RETURN)
 
 
-#'https://sg.linkedin.com/in/profilename'
-
-#linkedin_urls = driver.find_elements_by_class_name('yuRUbf')
 
 src = driver.page_source
 soup = BeautifulSoup(src, 'lxml')
@@ -54,9 +44,6 @@ for i in range(1, total_height, 4):
 
 time.sleep(0.8)
 
-
-# linkedin_urls = soup.findAll('a', href = True)
-#linkedin_urls = linkedin_urls.find_all('div', {'class':'yuRUbf'})
 linkedin_urls = []
 
 for link in soup.findAll('a', href = True):
